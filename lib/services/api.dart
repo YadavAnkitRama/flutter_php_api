7c91efd5edb5456a4a78f8dfee87f178e111e1b2
YadavankitRama <ankit.rama.yadav@outlook.com>
2020-06-26 04:54:37 +0530

    Login Page with PHP api Created

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Future loginUser(String email,String password) async {
-  String url = 'https://surajyadavapi123.000webhostapp.com/flutter-php-api.php';
+  String url = 'https://surajyadavapi123.000webhostapp.com/flutter-login.php';
   final response =await http.post(
     url,
     headers : {"Accept" : "Application/json"},
